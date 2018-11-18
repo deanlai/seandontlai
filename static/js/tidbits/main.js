434ae2e13f2724532c6ae554d31b3c016a587c30
Sean Lai <sean.j.lai@gmail.com>
2018-11-17 19:29:58 -0800

    excelled antifragile availabilities

---
+++ static/js/tidbits/main.js
@@ -0,0 +1,25 @@
+function flipCoin () {
+    var coin = Math.random()*2;
+    coin = coin < 1 ? "Heads" : "Tails";
+    document.getElementById("flip").innerHTML =
+    '<p class="subtitle has-text-centered">' + coin + '</p>';
+    setTimeout(function(){ document.getElementById("flip").innerHTML =
+    '<a class="subtitle has-text-centered" onclick="flipCoin()">Flip a coin</a>' }, 5000);
+}
+
+function checkBirthday() {
+    var d = new Date();
+    console.log("You'll have to wait till March. ¯\_(ツ)_/¯")
+    if (d.getDate() === 16 && d.getMonth() === 3) {
+        document.getElementById("birthday").innerHTML =
+        '<p class="subtitle has-text-centered" onclick="checkBirthday()">Yep</p>'
+        setTimeout(function(){ document.getElementById("birthday").innerHTML =
+        '<a class="subtitle has-text-centered" onclick="checkBirthday()">Is it Sean\'s birthday?</a>' }, 5000);
+    }
+    else {
+        document.getElementById("birthday").innerHTML =
+        '<p class="subtitle has-text-centered" onclick="checkBirthday()">Nope</p>'
+        setTimeout(function(){ document.getElementById("birthday").innerHTML =
+        '<a class="subtitle has-text-centered" onclick="checkBirthday()">Is it Sean\'s birthday?</a>' }, 5000);
+    }
+}
