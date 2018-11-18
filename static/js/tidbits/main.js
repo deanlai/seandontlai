5f6a2a837feb4b116c6ac1858755cf5bd9598d73
Sean Lai <sean.j.lai@gmail.com>
2018-11-17 21:07:56 -0800

    changed timing on tidbits refreshing.

--- static/js/tidbits/main.js
+++ static/js/tidbits/main.js
@@ -4,7 +4,7 @@ function flipCoin () {
     document.getElementById("flip").innerHTML =
     '<p class="subtitle has-text-centered">' + coin + '</p>';
     setTimeout(function(){ document.getElementById("flip").innerHTML =
-    '<a class="subtitle has-text-centered" onclick="flipCoin()">Flip a coin</a>' }, 5000);
+    '<a class="subtitle has-text-centered" onclick="flipCoin()">Flip a coin</a>' }, 1200);
 }
 
 function checkBirthday() {
@@ -14,12 +14,12 @@ function checkBirthday() {
         document.getElementById("birthday").innerHTML =
         '<p class="subtitle has-text-centered" onclick="checkBirthday()">Yep</p>'
         setTimeout(function(){ document.getElementById("birthday").innerHTML =
-        '<a class="subtitle has-text-centered" onclick="checkBirthday()">Is it Sean\'s birthday?</a>' }, 5000);
+        '<a class="subtitle has-text-centered" onclick="checkBirthday()">Is it Sean\'s birthday?</a>' }, 1200);
     }
     else {
         document.getElementById("birthday").innerHTML =
         '<p class="subtitle has-text-centered" onclick="checkBirthday()">Nope</p>'
         setTimeout(function(){ document.getElementById("birthday").innerHTML =
-        '<a class="subtitle has-text-centered" onclick="checkBirthday()">Is it Sean\'s birthday?</a>' }, 5000);
+        '<a class="subtitle has-text-centered" onclick="checkBirthday()">Is it Sean\'s birthday?</a>' }, 1200);
     }
 }
