15678e3e8fd5dc678f24aa10d28fb12e40c0c4d4
Sean Lai <sean.j.lai@gmail.com>
2018-12-09 14:17:10 -0800

    filtered optional partnerships

--- static/js/tidbits/main.js
+++ static/js/tidbits/main.js
@@ -23,3 +23,18 @@ function checkBirthday() {
         '<a class="subtitle has-text-centered" onclick="checkBirthday()">Is it Sean\'s birthday?</a>' }, 1200);
     }
 }
+
+function toggleVideo() {
+    var x = document.getElementById("crane");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (document.getElementById("cranetoggle").innerHTML === 'A Crane Flies') {
+        document.getElementById("cranetoggle").innerHTML = 'close'
+    }
+    else {
+        document.getElementById("cranetoggle").innerHTML = 'A Crane Flies'
+    }
+}
