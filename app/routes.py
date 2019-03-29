9135e90b1927c6b87b588133238cea42bb57f6e8
Sean Lai <sean.j.lai@gmail.com>
2019-03-28 19:15:08 -0700

    finished flask tutorial part III: webforms. Files updated.

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
-@app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
@@ -12,6 +12,16 @@ def home():
 @app.route('/tidbits')
 def tidbits():
     return render_template('tidbits.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('tidbits'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/places')
