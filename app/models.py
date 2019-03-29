469d22a1b9992a31db4392330d3f4d8706d16cc9
Sean Lai <sean.j.lai@gmail.com>
2019-03-29 10:36:46 -0700

    finished db and login chapter and added login/logout/registration.

---
+++ app/models.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db, login
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    problems = db.relationship('Problem', backref='setter', lazy='dynamic')
+
+    # Password hashing
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<Setter: {}>'.format(self.username)
+
+
+class Problem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    color = db.Column(db.String(32), index=True)
+    grade = db.Column(db.Integer, index=True)
+
+    def __repr__(self):
+        return '<Problem: {} {} {}>'.format(
+            self.grade, self.color, self.setter.username)
